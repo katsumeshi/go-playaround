b4c03d14e3ce2846f239da3de1fd5f8066806eab
Yuki Matsushita <ymatsushita@picniic.com>
2017-10-03 22:54:23 -0700

    mess aroundstring

--- foundation.go
+++ foundation.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var enabled, disabled = true, false // global
+
 func main() {
 
 	// Define variables -----
@@ -18,4 +20,57 @@ func main() {
 
 	_, b := 34, 35      // _ : any value that is given to it will be ignored.
 	fmt.Printf("%d", b) // 35
+
+	// Constants -----
+
+	const Pi = 3.14
+	const Pipi = Pi
+	const Ottupi = Pipi
+	const prefix = "tikubi"
+	fmt.Printf("%f\n", Pi)     // 353.140000????
+	fmt.Printf("%v\n", prefix) // tikubi%
+
+	// Boolean -----
+
+	fmt.Printf("%v\n", enabled) // tikubi%
+
+	// var enabled = false can't assign to global variable
+
+	// Numerical types
+	// can't compute diffrent integer type
+	// var a int8 = 1
+	// var b int32 = 2
+	// c := a + b
+
+	var c complex64 = 5 + 5i
+	//output: (5+5i)
+	fmt.Printf("Value is: %v\n", c)
+
+	// String -----
+
+	japaneseHello := "Ohaiou" // WTF ohayou is correct lol
+	fmt.Printf("Value is: %v\n", japaneseHello)
+
+	// var s string = "hello"
+	// s[0] = 'c'
+
+	s := "hello"
+	as := []byte(s) // convert string to []byte type
+	as[0] = 'c'
+	s2 := string(as) // convert back to string type
+	fmt.Printf("%s\n", s2)
+
+	s1 := "hello,"
+	m1 := " world"
+	a1 := s1 + m1
+	fmt.Printf("%s\n", a1)
+
+	s3 := "hello"
+	s3 = "c" + s3[1:] // you cannot change string values by index, but you can get values instead.
+	fmt.Printf("%s\n", s3)
+
+	m := `hello
+    world`
+	fmt.Printf("%s\n", m) // ` will not escape any characters in a string.
+
 }
